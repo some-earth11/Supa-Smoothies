78047b197bc00e17857995046c74e1d3a7ab15c3
some-earth11 <sk8762@srmist.edu.in>
2022-09-01 16:50:23 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+// pages
+import Home from "./pages/Home"
+import Create from "./pages/Create"
+import Update from "./pages/Update"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>Supa Smoothies</h1>
+        <Link to="/">Home</Link>
+        <Link to="/create">Create New Smoothie</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
