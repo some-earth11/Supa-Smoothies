740e429401c66485dd6522d517103a08a8d3ca17
some-earth11 <sk8762@srmist.edu.in>
2022-09-16 17:23:49 +0530

    added delete function

--- src/components/SmoothieCard.jsx
+++ src/components/SmoothieCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Delete from "./Delete"
 
 const SmoothieCard = ({ smoothie }) => {
     return (
@@ -10,6 +11,7 @@ const SmoothieCard = ({ smoothie }) => {
           <Link to={'/' + smoothie.id}>
             <i className="material-icons">edit</i>
           </Link>
+          <Delete key={smoothie.id} smoothie={smoothie} />
         </div>
       </div>
     )
